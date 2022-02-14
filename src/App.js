411fcd76cbc5e70927eab270211dc7fb2e7d9fec
Robert S <robscafe333@yahoo.com>
2022-02-13 22:00:03 -0800

    Practice with setting state in the constructor and passing it to an h1 element.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
 import React from "react";
-import reactDom from "react-dom";
-import todoData from "./todoData";
-import TodoItem from "./Components/TodoItem";
 
-
-// #1
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header username = "Robert"/>
-        <Greeting />
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      answer: "Yes",
+    };
   }
-}
-
-//#2
-class Header extends React.Component {
   render() {
-    return (
-      <header>
-        <p>Welcome, {this.props.username}!</p>
-      </header>
-    );
-  }
-}
-
-class Greeting extends React.Component {
-  render() {
-    const date = new Date();
-    const hours = date.getHours();
-    let timeOfDay;
-
-    if (hours < 12) {
-      timeOfDay = "morning";
-    } else if (hours >= 12 && hours < 17) {
-      timeOfDay = "afternoon";
-    } else {
-      timeOfDay = "night";
-    }
-    return <h1>Good {timeOfDay} to you, sir or madam!</h1>;
+    return <h1>is state important? {this.state.answer}</h1>;
   }
 }
 
